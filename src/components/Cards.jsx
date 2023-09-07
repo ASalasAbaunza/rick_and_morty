4403df3671364b94d63a89bd6989496c1013c948
ASalasAbaunza <alex.aba2409@gmail.com>
2023-09-06 21:30:46 -0600

    [Primer paso del proyecto]

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+export default function Cards(props) {
+   const cartas = props.characters.map(function (character) {
+      return (<Card key={character.id}
+         name={character.name}
+         status={character.status}
+         species={character.species}
+         gender={character.gender}
+         origin={character.origin.name}
+         image={character.image}
+         onClose={() => window.alert('Emulamos que se cierra la card')} />);
+   });
+   return (
+      <div>
+         {cartas}
+      </div>
+   );
+}
