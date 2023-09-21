25bf7705877ed7d0472ab161b3e367d0c923a8e0
ASalasAbaunza <alex.aba2409@gmail.com>
2023-09-20 23:01:28 -0600

    [Formulario de ingreso añadido]

--- src/App.js
+++ src/App.js
@@ -1,50 +1,77 @@
 import React from 'react';
-import SearchBar from './components/SearchBar.jsx';
 import Home from './components/Home.jsx'
 import About from './components/About.jsx';
 import Detail from './components/Detail.jsx';
-import {Routes, Route} from 'react-router-dom';
-import { useState } from 'react';
+import Form from './components/Form.jsx';
+import Nav from './components/Nav.jsx';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
    let [characters, setCharacters] = useState([]);
     //Estamos creando un estado y una función para modificar el estado
+   const location = useLocation();
 
-    const onSearch = (id) => {
-        fetch(`https://rickandmortyapi.com/api/character/${id}`)
-            .then((res) => res.json())
-            .then((data) => {
-                let printed = false;
-                for (let i=0; i<characters.length;i++) {
-                    if (characters[i].id === data.id) {
-                    printed = true;
-                    }
-                 }
-                if (printed) {
-                    window.alert("Character is already there!");
-                } else {
-                    setCharacters([...characters,data])
-                }
-        });
-    };
+   const navigate = useNavigate();
+   const [access, setAccess] = useState(false);
+   const email = 'alex.aba2409@gmail.com';
+   const password = 'morgenial1';
 
-const onClose = (id) => {
-   let newCharacters = [];
-   for (let i=0; i<characters.length; i++) {
-      if (characters[i].id !== id) {
-         newCharacters.push(characters[i]);
+   const login = (form) => {
+      if (form.email === email && form.password === password) {
+         setAccess(true);
+         navigate('/home');
+      } else {
+         alert('Check your login details!')
       }
-   }
-   characters = newCharacters;
-   setCharacters(characters);
-}
+   };
+
+   const logout = () => {
+      setAccess(false);
+   };
+
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
+
+   const onSearch = (id) => {
+      fetch(`https://rickandmortyapi.com/api/character/${id}`)
+         .then((res) => res.json())
+         .then((data) => {
+            let printed = false;
+            for (let i=0; i<characters.length;i++) {
+               if (characters[i].id === data.id) {
+                  printed = true;
+               }
+            }
+               if (printed) {
+                  window.alert("Character is already there!");
+               } else {
+                  setCharacters([...characters,data])
+               }
+         });
+   };
+
+   const onClose = (id) => {
+      let newCharacters = [];
+      for (let i=0; i<characters.length; i++) {
+         if (characters[i].id !== id) {
+            newCharacters.push(characters[i]);
+         }
+      }
+      characters = newCharacters;
+      setCharacters(characters);
+   };
 
    return (
       <div>
-         <div style={{backgroundColor: 'darkgray'}}>
-            <SearchBar onSearch={onSearch} />
-         </div>
+         {location.pathname!=='/' && (
+            <div style={{backgroundColor: 'darkgray'}}>
+               <Nav onSearch={onSearch} logout={logout}/>
+            </div>
+         )}
          <Routes>
+            <Route path='/' element={<Form login={login}/>}/>
             <Route path='/home' element={<Home characters={characters} onClose={onClose} />} />
             <Route path='/about' element={<About/>} />
             <Route path='/detail/:id' element={<Detail/>} />
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./ComponentStyles.module.css";
+import SearchBar from "./SearchBar";
+import { NavLink } from "react-router-dom";
+
+export default function Nav(props) {
+   return (
+      <div style={{display: "flex", justifyContent: "space-evenly", flexDirection: "row", alignItems: "center"}}>
+        <button className={styles.botonNav} >
+            <NavLink to='/about'>About</NavLink>
+        </button>
+        <button className={styles.botonNav} >
+            <NavLink to='/home'>Home</NavLink>
+        </button>
+        <SearchBar onSearch={props.onSearch}/>
+        <button className={styles.botonNav} onClick={props.logout}>Log Out</button>
+      </div>
+   );
+}
