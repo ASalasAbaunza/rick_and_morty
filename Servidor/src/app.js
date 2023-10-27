f3bed3d279cd58af75338d5bd2cb330e0000aab0
ASalasAbaunza <alex.aba2409@gmail.com>
2023-10-26 22:14:49 -0600

    [Tests añadidos]

---
+++ Servidor/src/app.js
@@ -0,0 +1,45 @@
+/*const http = require('http');
+const { getCharById } = require('./controllers/getCharById');
+
+http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.url.includes('/rickandmorty/character')) {
+        let id = '';
+        for (let i=req.url.length-1; i>0; i--) {
+            if (req.url.charCodeAt(i) !== 47) {
+                id = req.url[i] + id;
+            } else {
+                break;
+            }
+        }
+        id = parseInt(id);
+        getCharById(res,id);
+    }
+}).listen(3001, 'localhost');*/
+
+const express = require('express');
+const { routeGetCharById, routePostFav, routeDeleteFav, routeLogin } = require('./routes/index');
+const server = express();
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+ });
+
+ server.use(express.json());
+
+ server.use('/rickandmorty', routeGetCharById);
+ server.use('/rickandmorty', routePostFav);
+ server.use('/rickandmorty', routeDeleteFav);
+ server.use('/rickandmorty', routeLogin);
+
+ module.exports = server;
---
+++ Servidor/test/index.test.js
@@ -0,0 +1,83 @@
+const app = require('../src/app');
+const session = require('supertest');
+const users = require('../src/utils/users');
+const agent = session(app);
+
+describe('Test de Rutas', () => {
+    it('Debe responder un GET en la ruta /rickandmorty/character/:id con un code 200', async () => {
+        const res = await agent.get('/rickandmorty/character/1');
+        expect(res.statusCode).toBe(200);
+    });
+    it('Debe responder un POST en la ruta /rickandmorty/fav con un code 200 si lleva datos', async () => {
+        const res = await agent.post('/rickandmorty/fav').send({id: 1, name: 'Rick Sánchez'});
+        expect(res.statusCode).toBe(200);
+    });
+    it('Debe responder un DELETE en la ruta /rickandmorty/fav/:id con un code 200', async () => {
+        const res = await agent.delete('/rickandmorty/fav/1');
+        expect(res.statusCode).toBe(200);
+    });
+    it('Debe responder un GET en la ruta /rickandmorty/login con un code 200', async () => {
+        const res = await agent.get('/rickandmorty/login');
+        expect(res.statusCode).toBe(200);
+    });
+});
+
+describe('GET /rickandmorty/character/:id', () => {
+    it('Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image"', async () => {
+        const res = await agent.get('/rickandmorty/character/1');
+        expect(typeof res.body).toBe('object');
+        expect(res.body).toHaveProperty('id');
+        expect(res.body).toHaveProperty('name');
+        expect(res.body).toHaveProperty('species');
+        expect(res.body).toHaveProperty('gender');
+        expect(res.body).toHaveProperty('status');
+        expect(res.body).toHaveProperty('origin');
+        expect(res.body).toHaveProperty('image');
+    });
+    it('Si hay un error responde con status: 500', async () => {
+        const res = await agent.get('/rickandmorty/character/999');
+        expect(res.statusCode).toBe(500);
+    });
+});
+
+describe('GET /rickandmorty/login', () => {
+    it('Si el user es correcto, debe dar acceso', async () => {
+        const res = await agent.get(`/rickandmorty/login/?email=${users[0].email}&&password=${users[0].password}`);
+        expect(res.statusCode).toBe(200);
+        expect(typeof res.body).toBe('object');
+        expect(res.body).toEqual({access: true});
+    });
+    it('Si el user es incorrecto, debe negar el acceso', async () => {
+        const res = await agent.get('/rickandmorty/login');
+        expect(res.statusCode).toBe(200);
+        expect(typeof res.body).toBe('object');
+        expect(res.body).toEqual({access: false});
+    });
+});
+
+xdescribe('POST /rickandmorty/fav', () => {
+    it('Los datos que se reciben, se devuelven dentro de un arreglo', async () => {
+        const res = await agent.post('/rickandmorty/fav').send({id: 1, name: 'Rick Sánchez'});
+        expect(Array.isArray(res.body)).toBe(true);
+        expect(res.body).toEqual([{id: 1, name: 'Rick Sánchez'}]);
+    });
+    it('Si se vuelve a hacer la petición, los nuevos datos se añaden al arreglo', async () => {
+        const res = await agent.post('/rickandmorty/fav').send({id: 2, name: 'Morty Smith'});
+        expect(Array.isArray(res.body)).toBe(true);
+        expect(res.body).toEqual([{id: 1, name: 'Rick Sánchez'}, {id: 2, name: 'Morty Smith'}]);
+    });
+});
+
+describe('DELETE /rickandmorty/fav/:id', () => {
+    it('Si se manda un ID que no está en favoritos, devuelve el mismo arreglo', async () => {
+        const res1 = await agent.post('/rickandmorty/fav').send({id: 3, name: 'Summer Smith'});
+        const res2 = await agent.delete('/rickandmorty/fav/4');
+        expect(res2.statusCode).toBe(200);
+        expect(res2.body).toEqual(res1.body);
+    });
+    xit('Si se manda un ID que sí está en favoritos, se elimina del arreglo', async () => {
+        const res3 = await agent.delete('/rickandmorty/fav/1');
+        expect(res3.statusCode).toBe(200);
+        expect(res3.body).toEqual([{id: 2, name: 'Morty Smith'}, {id: 3, name: 'Summer Smith'}]);
+    });
+});
